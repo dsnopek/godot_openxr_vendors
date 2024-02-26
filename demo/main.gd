a1699cec83d47093c93d58bb499df584779d6ed3
David Snopek <dsnopek@gmail.com>
2024-02-26 13:54:48 -0600

    Allow switching to/from passthrough

--- demo/main.gd
+++ demo/main.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var left_hand_mesh: MeshInstance3D = $XROrigin3D/LeftHand/LeftHandMesh
 @onready var right_hand_mesh: MeshInstance3D = $XROrigin3D/RightHand/RightHandMesh
+@onready var floor_mesh: MeshInstance3D = $Floor
+@onready var world_environment: WorldEnvironment = $WorldEnvironment
 
 var xr_interface : XRInterface = null
 
@@ -11,10 +13,12 @@ func _ready():
 	if xr_interface and xr_interface.is_initialized():
 		var vp: Viewport = get_viewport()
 		vp.use_xr = true
+		print("blend modes: ", xr_interface.get_supported_environment_blend_modes())
 
 	var scene_capture := Engine.get_singleton("OpenXRFbSceneCaptureExtensionWrapper")
 	if scene_capture:
 		scene_capture.connect("scene_capture_completed", _on_scene_capture_completed)
+
 
 
 func _on_scene_capture_completed():
@@ -27,6 +31,26 @@ func _on_left_hand_button_pressed(name):
 		print("Triggering scene capture")
 		scene_capture.request_scene_capture()
 
+	if name == "by_button":
+		var supported_blend_modes = xr_interface.get_supported_environment_blend_modes()
+		print ("Supported blend modes: ", supported_blend_modes)
+		if XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND in supported_blend_modes and XRInterface.XR_ENV_BLEND_MODE_OPAQUE in supported_blend_modes:
+			print ("Passthrough supported.")
+			if xr_interface.environment_blend_mode == XRInterface.XR_ENV_BLEND_MODE_OPAQUE:
+				# Switch to passthrough.
+				xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND
+				get_viewport().transparent_bg = true
+				world_environment.environment.background_mode = Environment.BG_COLOR
+				world_environment.environment.background_color = Color(0.0, 0.0, 0.0, 0.0)
+				floor_mesh.visible = false
+			elif xr_interface.environment_blend_mode == XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND:
+				# Switch back to VR.
+				xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_OPAQUE
+				get_viewport().transparent_bg = false
+				world_environment.environment.background_mode = Environment.BG_SKY
+				floor_mesh.visible = true
+		else:
+			print("Switching to/from passthrough not supported.")
 
 func _on_left_controller_fb_render_model_render_model_loaded() -> void:
 	left_hand_mesh.hide()
