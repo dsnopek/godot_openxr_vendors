fd2e42f29c07f95496b5d3ef22e0f75e8954255a
David Snopek <dsnopek@gmail.com>
2024-02-29 10:30:47 -0600

    Finish out the properties and methods scaffolding for OpenXRFbSceneManager

--- common/src/main/cpp/classes/openxr_fb_scene_manager.cpp
+++ common/src/main/cpp/classes/openxr_fb_scene_manager.cpp
@@ -29,15 +29,48 @@
 
 #include "classes/openxr_fb_scene_manager.h"
 
+#include <godot_cpp/classes/open_xr_interface.hpp>
+#include <godot_cpp/classes/xr_origin3d.hpp>
+#include <godot_cpp/classes/xr_anchor3d.hpp>
+#include <godot_cpp/classes/xr_server.hpp>
+
 #include "extensions/openxr_fb_scene_extension_wrapper.h"
 
 using namespace godot;
+
+void OpenXRFbSceneManager::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_default_scene", "packed_scene"), &OpenXRFbSceneManager::set_default_scene);
+	ClassDB::bind_method(D_METHOD("get_default_scene"), &OpenXRFbSceneManager::get_default_scene);
+
+	ClassDB::bind_method(D_METHOD("set_scene_setup_method", "method_name"), &OpenXRFbSceneManager::set_scene_setup_method);
+	ClassDB::bind_method(D_METHOD("get_scene_setup_method"), &OpenXRFbSceneManager::get_scene_setup_method);
+
+	ClassDB::bind_method(D_METHOD("set_auto_create", "enable"), &OpenXRFbSceneManager::set_auto_create);
+	ClassDB::bind_method(D_METHOD("get_auto_create"), &OpenXRFbSceneManager::get_auto_create);
+
+	ClassDB::bind_method(D_METHOD("set_visible", "visible"), &OpenXRFbSceneManager::set_visible);
+	ClassDB::bind_method(D_METHOD("get_visible"), &OpenXRFbSceneManager::get_visible);
+	ClassDB::bind_method(D_METHOD("show"), &OpenXRFbSceneManager::show);
+	ClassDB::bind_method(D_METHOD("hide"), &OpenXRFbSceneManager::hide);
+
+	ClassDB::bind_method(D_METHOD("create_scene_anchors"), &OpenXRFbSceneManager::create_scene_anchors);
+	ClassDB::bind_method(D_METHOD("remove_scene_anchors"), &OpenXRFbSceneManager::remove_scene_anchors);
+	ClassDB::bind_method(D_METHOD("are_scene_anchors_created"), &OpenXRFbSceneManager::are_scene_anchors_created);
+
+	ClassDB::bind_method(D_METHOD("get_anchor_node", "uuid"), &OpenXRFbSceneManager::get_anchor_node);
+	ClassDB::bind_method(D_METHOD("get_anchor_nodes"), &OpenXRFbSceneManager::get_anchor_nodes);
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "default_scene", PROPERTY_HINT_RESOURCE_TYPE, "PackedScene"), "set_default_scene", "get_default_scene");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "scene_setup_method", PROPERTY_HINT_NONE, ""), "set_scene_setup_method", "get_scene_setup_method");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "auto_create", PROPERTY_HINT_NONE, ""), "set_auto_create", "get_auto_create");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "visible", PROPERTY_HINT_NONE, ""), "set_visible", "get_visible");
+}
 
 bool OpenXRFbSceneManager::_set(const StringName &p_name, const Variant &p_value) {
 	PackedStringArray parts = p_name.split("/", true, 2);
 	if (parts[0] == "scenes") {
 		const PackedStringArray &semantic_labels = OpenXRFbSceneExtensionWrapper::get_supported_semantic_labels();
-		if (parts[1] == "default" || semantic_labels.has(parts[1])) {
+		if (semantic_labels.has(parts[1])) {
 			scenes[parts[1]] = p_value;
 			return true;
 		}
@@ -49,7 +82,7 @@ bool OpenXRFbSceneManager::_get(const StringName &p_name, Variant &r_ret) const 
 	PackedStringArray parts = p_name.split("/", true, 2);
 	if (parts[0] == "scenes") {
 		const PackedStringArray &semantic_labels = OpenXRFbSceneExtensionWrapper::get_supported_semantic_labels();
-		if (parts[1] == "default" || semantic_labels.has(parts[1])) {
+		if (semantic_labels.has(parts[1])) {
 			const Ref<PackedScene> *scene = scenes.getptr(p_name.substr(7));
 			r_ret = scene ? Variant(*scene) : Variant();
 			return true;
@@ -60,11 +93,135 @@ bool OpenXRFbSceneManager::_get(const StringName &p_name, Variant &r_ret) const 
 
 void OpenXRFbSceneManager::_get_property_list(List<PropertyInfo> *p_list) const {
 	const PackedStringArray &semantic_labels = OpenXRFbSceneExtensionWrapper::get_supported_semantic_labels();
-	p_list->push_back(PropertyInfo(Variant::Type::OBJECT, "scenes/default", PROPERTY_HINT_RESOURCE_TYPE, "PackedScene"));
 	for (int i = 0; i < semantic_labels.size(); i++) {
 		p_list->push_back(PropertyInfo(Variant::Type::OBJECT, "scenes/" + semantic_labels[i].to_lower(), PROPERTY_HINT_RESOURCE_TYPE, "PackedScene"));
 	}
 }
 
-void OpenXRFbSceneManager::_bind_methods() {
+void OpenXRFbSceneManager::_notification(int p_what) {
+	switch (p_what) {
+		case NOTIFICATION_ENTER_TREE: {
+			Ref<OpenXRInterface> openxr_interface = XRServer::get_singleton()->find_interface("OpenXR");
+			if (openxr_interface.is_valid()) {
+				openxr_interface->connect("session_begun", callable_mp(this, &OpenXRFbSceneManager::_on_openxr_session_begun));
+				openxr_interface->connect("session_stopping", callable_mp(this, &OpenXRFbSceneManager::_on_openxr_session_stopping));
+			}
+
+			xr_origin = Object::cast_to<XROrigin3D>(get_parent());
+			if (xr_origin && auto_create && openxr_interface.is_valid() && openxr_interface->is_initialized()) {
+				create_scene_anchors();
+			}
+		} break;
+		case NOTIFICATION_EXIT_TREE: {
+			Ref<OpenXRInterface> openxr_interface = XRServer::get_singleton()->find_interface("OpenXR");
+			if (openxr_interface.is_valid()) {
+				openxr_interface->disconnect("session_begun", callable_mp(this, &OpenXRFbSceneManager::_on_openxr_session_begun));
+				openxr_interface->disconnect("session_stopping", callable_mp(this, &OpenXRFbSceneManager::_on_openxr_session_stopping));
+			}
+
+			if (xr_origin && anchors_created) {
+				remove_scene_anchors();
+			}
+		} break;
+	}
+}
+
+void OpenXRFbSceneManager::_on_openxr_session_begun() {
+	if (xr_origin && auto_create && !anchors_created) {
+		create_scene_anchors();
+	}
+}
+
+void OpenXRFbSceneManager::_on_openxr_session_stopping() {
+	if (anchors_created) {
+		remove_scene_anchors();
+	}
+}
+
+PackedStringArray OpenXRFbSceneManager::_get_configuration_warnings() const {
+	PackedStringArray warnings = Node::_get_configuration_warnings();
+
+	if (is_inside_tree()) {
+		XROrigin3D *origin = Object::cast_to<XROrigin3D>(get_parent());
+		if (origin == nullptr) {
+			warnings.push_back("Must be a child of XROrigin3D");
+		}
+	}
+
+	return warnings;
+}
+
+void OpenXRFbSceneManager::set_default_scene(const Ref<PackedScene> &p_default_scene) {
+	default_scene = p_default_scene;
+}
+
+Ref<PackedScene> OpenXRFbSceneManager::get_default_scene() const {
+	return default_scene;
+}
+
+void OpenXRFbSceneManager::set_scene_setup_method(const StringName &p_method) {
+	scene_setup_method = p_method;
+}
+
+StringName OpenXRFbSceneManager::get_scene_setup_method() const {
+	return scene_setup_method;
+}
+
+void OpenXRFbSceneManager::set_auto_create(bool p_auto_create) {
+	auto_create = p_auto_create;
+}
+
+bool OpenXRFbSceneManager::get_auto_create() const {
+	return auto_create;
+}
+
+void OpenXRFbSceneManager::set_visible(bool p_visible) {
+	visible = p_visible;
+	// @todo Loop over anchors and change their visibility
+}
+
+bool OpenXRFbSceneManager::get_visible() const {
+	return visible;
+}
+
+void OpenXRFbSceneManager::show() {
+	set_visible(true);
+}
+
+void OpenXRFbSceneManager::hide() {
+	set_visible(false);
+}
+
+void OpenXRFbSceneManager::create_scene_anchors() {
+	ERR_FAIL_COND(anchors_created);
+	ERR_FAIL_COND(!xr_origin);
+
+	// @todo it!
+
+	anchors_created = true;
+}
+
+void OpenXRFbSceneManager::remove_scene_anchors() {
+	ERR_FAIL_COND(!anchors_created);
+	ERR_FAIL_COND(!xr_origin);
+
+	// @todo it!
+
+	anchors_created = false;
+}
+
+bool OpenXRFbSceneManager::are_scene_anchors_created() const {
+	return anchors_created;
+}
+
+XRAnchor3D *OpenXRFbSceneManager::get_anchor_node(const StringName &p_uuid) const {
+	ERR_FAIL_COND_V(!anchors_created, nullptr);
+	// @todo it!
+	return nullptr;
+}
+
+Array OpenXRFbSceneManager::get_anchor_nodes() const {
+	ERR_FAIL_COND_V(!anchors_created, Array());
+	// @todo it!
+	return Array();
 }
--- common/src/main/cpp/include/classes/openxr_fb_scene_manager.h
+++ common/src/main/cpp/include/classes/openxr_fb_scene_manager.h
@@ -38,22 +38,58 @@
 
 namespace godot {
 
+class XROrigin3D;
+class XRAnchor3D;
+
 class OpenXRFbSceneManager : public Node {
 	GDCLASS(OpenXRFbSceneManager, Node);
 
-public:
-
-private:
+	Ref<PackedScene> default_scene;
+	StringName scene_setup_method = "setup_scene";
+	bool auto_create = true;
+	bool visible = true;
 	HashMap<StringName, Ref<PackedScene>> scenes;
+
+	XROrigin3D *xr_origin = nullptr;
+
+	bool anchors_created = false;
+	HashMap<StringName, ObjectID> anchor_nodes;
 
 protected:
 	bool _set(const StringName &p_name, const Variant &p_value);
 	bool _get(const StringName &p_name, Variant &r_ret) const;
 	void _get_property_list(List<PropertyInfo> *p_list) const;
 
+	void _notification(int p_what);
+
+	void _on_openxr_session_begun();
+	void _on_openxr_session_stopping();
+
 	static void _bind_methods();
 
 public:
+	PackedStringArray _get_configuration_warnings() const override;
+
+	void set_default_scene(const Ref<PackedScene> &p_default_scene);
+	Ref<PackedScene> get_default_scene() const;
+
+	void set_scene_setup_method(const StringName &p_method);
+	StringName get_scene_setup_method() const;
+
+	void set_auto_create(bool p_auto_create);
+	bool get_auto_create() const;
+
+	void set_visible(bool p_visible);
+	bool get_visible() const;
+	void show();
+	void hide();
+
+	void create_scene_anchors();
+	void remove_scene_anchors();
+	bool are_scene_anchors_created() const;
+
+	XRAnchor3D *get_anchor_node(const StringName &p_uuid) const;
+	Array get_anchor_nodes() const;
 };
 
 } // namespace godot
