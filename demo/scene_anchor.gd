e22449fa52b3a6ff9f3a479c2c337a1aea8b80e8
David Snopek <dsnopek@gmail.com>
2024-03-05 17:49:02 -0600

    Resize mesh for bounded 3D anchors

--- demo/scene_anchor.gd
+++ demo/scene_anchor.gd
@@ -1,4 +1,11 @@
 extends Node3D
 
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
+
 func setup_scene(entity: OpenXRFbSpatialEntity) -> void:
-	print ("I'm getting setup!")
+	if entity.is_component_enabled(OpenXRFbSpatialEntity.COMPONENT_TYPE_BOUNDED_3D):
+		var bb: AABB = entity.get_bounding_box_3d()
+		var mesh: BoxMesh = BoxMesh.new()
+		mesh.size = bb.size
+		mesh_instance.mesh = mesh
+		mesh_instance.position = bb.get_center()
