d57232c33367f2f60c3719440bdfc1a2a8c63f59
David Snopek <dsnopek@gmail.com>
2024-03-12 14:40:49 -0500

    Add support for developer-created spatial anchors via XR_FB_spatial_entity

--- demo/main.gd
+++ demo/main.gd
@@ -4,22 +4,41 @@ extends Node3D
 @export var passthrough_curve: Curve
 @export var bcs: Vector3
 
-var xr_interface: XRInterface = null
-var hand_tracking_source: Array[OpenXRInterface.HandTrackedSource]
-var passthrough_enabled: bool = false
-
 @onready var left_hand: XRController3D = $XROrigin3D/LeftHand
 @onready var right_hand: XRController3D = $XROrigin3D/RightHand
 @onready var left_hand_mesh: MeshInstance3D = $XROrigin3D/LeftHand/LeftHandMesh
 @onready var right_hand_mesh: MeshInstance3D = $XROrigin3D/RightHand/RightHandMesh
 @onready var left_controller_model: OpenXRFbRenderModel = $XROrigin3D/LeftHand/LeftControllerFbRenderModel
 @onready var right_controller_model: OpenXRFbRenderModel = $XROrigin3D/RightHand/RightControllerFbRenderModel
+@onready var left_hand_pointer: XRController3D = $XROrigin3D/LeftHandPointer
+@onready var left_hand_pointer_raycast: RayCast3D = $XROrigin3D/LeftHandPointer/RayCast3D
+@onready var scene_pointer_mesh: MeshInstance3D = $XROrigin3D/LeftHandPointer/ScenePointerMesh
+@onready var scene_colliding_mesh: MeshInstance3D = $XROrigin3D/LeftHandPointer/SceneCollidingMesh
 @onready var floor_mesh: MeshInstance3D = $Floor
 @onready var world_environment: WorldEnvironment = $WorldEnvironment
 @onready var scene_manager: OpenXRFbSceneManager = $XROrigin3D/OpenXRFbSceneManager
+@onready var spatial_anchor_manager: OpenXRFbSpatialAnchorManager = $XROrigin3D/OpenXRFbSpatialAnchorManager
 @onready var open_xr_fb_passthrough_geometry: OpenXRFbPassthroughGeometry = %OpenXRFbPassthroughGeometry
 @onready var passthrough_mode_info: Label3D = $XROrigin3D/RightHand/PassthroughModeInfo
 @onready var passthrough_filter_info: Label3D = $XROrigin3D/RightHand/PassthroughFilterInfo
+
+const SpatialAnchor = preload("res://spatial_anchor.tscn")
+
+const COLORS = [
+	"#FF0000", # Red
+	"#00FF00", # Green
+	"#0000FF", # Blue
+	"#FFFF00", # Yellow
+	"#00FFFF", # Cyan
+	"#FF00FF", # Magenta
+	"#FF8000", # Orange
+	"#800080", # Purple
+]
+
+var xr_interface: XRInterface = null
+var hand_tracking_source: Array[OpenXRInterface.HandTrackedSource]
+var passthrough_enabled: bool = false
+var selected_spatial_anchor_node: Node3D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,6 +50,9 @@ func _ready():
 	hand_tracking_source.resize(OpenXRInterface.HAND_MAX)
 	for hand in OpenXRInterface.HAND_MAX:
 		hand_tracking_source[hand] = xr_interface.get_hand_tracking_source(hand)
+
+	randomize()
+
 
 func enable_passthrough(enable: bool) -> void:
 	if passthrough_enabled == enable:
@@ -48,6 +70,9 @@ func enable_passthrough(enable: bool) -> void:
 			world_environment.environment.background_color = Color(0.0, 0.0, 0.0, 0.0)
 			floor_mesh.visible = false
 			scene_manager.visible = true
+			spatial_anchor_manager.visible = true
+			left_hand_pointer.visible = true
+			left_hand_pointer_raycast.enabled = true
 			if not scene_manager.are_scene_anchors_created():
 				scene_manager.create_scene_anchors()
 		else:
@@ -57,9 +82,13 @@ func enable_passthrough(enable: bool) -> void:
 			world_environment.environment.background_mode = Environment.BG_SKY
 			floor_mesh.visible = true
 			scene_manager.visible = false
+			spatial_anchor_manager.visible = false
+			left_hand_pointer.visible = false
+			left_hand_pointer_raycast.enabled = false
 		passthrough_enabled = enable
 	else:
 		print("Switching to/from passthrough not supported.")
+
 
 func _physics_process(_delta: float) -> void:
 	for hand in OpenXRInterface.HAND_MAX:
@@ -87,6 +116,37 @@ func _physics_process(_delta: float) -> void:
 
 		hand_tracking_source[hand] = source
 
+	if left_hand_pointer.visible:
+		var previous_selected_spatial_anchor_node = selected_spatial_anchor_node
+
+		if left_hand_pointer_raycast.is_colliding():
+			var collision_point: Vector3 = left_hand_pointer_raycast.get_collision_point()
+			scene_colliding_mesh.global_position = collision_point
+
+			var pointer_length: float = (collision_point - left_hand_pointer.global_position).length()
+			scene_pointer_mesh.mesh.size.z = pointer_length
+			scene_pointer_mesh.position.z = -pointer_length / 2.0
+
+			var collider: CollisionObject3D = left_hand_pointer_raycast.get_collider()
+			if collider.get_collision_layer_value(3):
+				selected_spatial_anchor_node = collider
+			else:
+				selected_spatial_anchor_node = null
+		else:
+			scene_pointer_mesh.mesh.size.z = 5
+			scene_pointer_mesh.position.z = -2.5
+			selected_spatial_anchor_node = null
+
+		if previous_selected_spatial_anchor_node != selected_spatial_anchor_node:
+			if previous_selected_spatial_anchor_node:
+				previous_selected_spatial_anchor_node.set_selected(false)
+			if selected_spatial_anchor_node:
+				selected_spatial_anchor_node.set_selected(true)
+				scene_colliding_mesh.visible = false
+			else:
+				scene_colliding_mesh.visible = true
+
+
 func _on_left_hand_button_pressed(name):
 	if name == "menu_button":
 		print("Triggering scene capture")
@@ -95,12 +155,35 @@ func _on_left_hand_button_pressed(name):
 	elif name == "by_button":
 		enable_passthrough(not passthrough_enabled)
 
+	elif name == "trigger_click" and left_hand_pointer.visible:
+		if left_hand_pointer_raycast.is_colliding():
+			if selected_spatial_anchor_node:
+				var anchor_parent = selected_spatial_anchor_node.get_parent()
+				if anchor_parent is XRAnchor3D:
+					spatial_anchor_manager.untrack_anchor(anchor_parent.tracker)
+			else:
+				var anchor_transform := Transform3D()
+				anchor_transform.origin = left_hand_pointer_raycast.get_collision_point()
+
+				var collision_normal: Vector3 = left_hand_pointer_raycast.get_collision_normal()
+				if collision_normal.is_equal_approx(Vector3.UP):
+					anchor_transform.basis = anchor_transform.basis.rotated(Vector3(1.0, 0.0, 0.0), PI / 2.0)
+				elif collision_normal.is_equal_approx(Vector3.DOWN):
+					anchor_transform.basis = anchor_transform.basis.rotated(Vector3(1.0, 0.0, 0.0), -PI / 2.0)
+				else:
+					anchor_transform.basis = Basis.looking_at(left_hand_pointer_raycast.get_collision_normal())
+
+				print ("Attempting to create spatial anchor at: ", anchor_transform)
+				spatial_anchor_manager.create_anchor(anchor_transform, { color = COLORS[randi() % COLORS.size()] })
+
+
 func _on_right_hand_button_pressed(name: String) -> void:
 	match name:
 		"by_button":
 			update_passthrough_mode()
 		"ax_button":
 			update_passthrough_filter()
+
 
 func _on_left_controller_fb_render_model_render_model_loaded() -> void:
 	left_hand_mesh.hide()
