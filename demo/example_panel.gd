6716e5e31dbbafe5f3cdd8e1b14176bdf333cd8c
David Snopek <dsnopek@gmail.com>
2024-04-05 16:06:33 -0500

    Implement cylinder intersects ray

--- demo/example_panel.gd
+++ demo/example_panel.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var pointer: ColorRect = $Pointer
+
 var start_pos: Vector2
 
 func _ready() -> void:
@@ -8,3 +10,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$Label.position = Vector2(start_pos.x + (100.0 * sin(Time.get_ticks_msec() * 0.001 * 2)), start_pos.y)
 
+func update_pointer(p_position: Vector2) -> void:
+	pointer.position = size * p_position
--- demo/main.gd
+++ demo/main.gd
@@ -52,6 +52,50 @@ func _physics_process(_delta: float) -> void:
 
 		hand_tracking_source[hand] = source
 
+	var ray_intersection: Vector2 = cylinder_intersects_ray($XROrigin3D/OpenXRCompositionLayerCylinder, right_hand.transform.origin, -$XROrigin3D/RightHandPointer.transform.basis.z)
+	#print ("Ray intersection: ", ray_intersection)
+	$SubViewport/Control.update_pointer(ray_intersection)
+
+func cylinder_intersects_ray(p_layer: OpenXRCompositionLayerCylinder, p_origin: Vector3, p_direction: Vector3) -> Vector2:
+	var cylinder_radius: float = p_layer.radius
+	var cylinder_angle: float = p_layer.central_angle
+	var cylinder_center: Vector3 = p_layer.position
+	var cylinder_axis: Vector3 = p_layer.transform.basis.y
+	var cylinder_aspect_ratio: float = p_layer.aspect_ratio
+
+	var offset: Vector3 = p_origin - cylinder_center
+	var a: float = p_direction.dot(p_direction - cylinder_axis * p_direction.dot(cylinder_axis))
+	var b: float = 2.0 * (p_direction.dot(offset - cylinder_axis * offset.dot(cylinder_axis)))
+	var c: float = offset.dot(offset - cylinder_axis * offset.dot(cylinder_axis)) - (cylinder_radius * cylinder_radius)
+
+	var discriminant: float = b * b - 4.0 * a * c
+	if discriminant < 0.0:
+		return Vector2(-1.0, -1.0)
+
+	var t0: float = (-b - sqrt(discriminant)) / (2.0 * a)
+	var t1: float = (-b + sqrt(discriminant)) / (2.0 * a)
+	var t: float = max(t0, t1)
+
+	if t < 0.0:
+		return Vector2(-1.0, -1.0)
+
+	var intersection: Vector3 = p_origin + p_direction * t
+	$IntersectionSphere.position = intersection
+	var relative_point: Vector3 = intersection - cylinder_center
+	var projected_point: Vector2 = Vector2(relative_point.x, relative_point.z)
+	var intersection_angle: float = (PI / 2.0) + atan2(projected_point.y, projected_point.x)
+	if abs(intersection_angle) > cylinder_angle / 2.0:
+		return Vector2(-1.0, -1.0)
+
+	var arc_length = cylinder_radius * cylinder_angle
+	var height = cylinder_aspect_ratio * arc_length
+	if abs(relative_point.y) > height / 2.0:
+		return Vector2(-1.0, -1.0)
+
+	var u: float = 0.5 + (intersection_angle / cylinder_angle)
+	var v: float = 1.0 - (0.5 + (relative_point.y / height))
+
+	return Vector2(u, v)
 
 func _on_scene_capture_completed():
 	print("Scene Capture completed")
