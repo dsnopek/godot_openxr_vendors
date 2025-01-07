36ab4572a92b9c64dd21efe158518377f8520761
David Snopek <dsnopek@gmail.com>
2025-01-07 15:25:36 -0600

    Initial boilerplate for depth extension

---
+++ plugin/src/main/cpp/extensions/openxr_meta_environment_depth_extension_wrapper.cpp
@@ -0,0 +1,104 @@
+/**************************************************************************/
+/*  openxr_meta_environment_depth_extension_wrapper.cpp                   */
+/**************************************************************************/
+/*                       This file is part of:                            */
+/*                              GODOT XR                                  */
+/*                      https://godotengine.org                           */
+/**************************************************************************/
+/* Copyright (c) 2022-present Godot XR contributors (see CONTRIBUTORS.md) */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "extensions/openxr_meta_environment_depth_extension_wrapper.h"
+
+#include <godot_cpp/classes/object.hpp>
+#include <godot_cpp/classes/open_xrapi_extension.hpp>
+#include <godot_cpp/templates/vector.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+using namespace godot;
+
+OpenXRMetaEnvironmentDepthExtensionWrapper *OpenXRMetaEnvironmentDepthExtensionWrapper::singleton = nullptr;
+
+OpenXRMetaEnvironmentDepthExtensionWrapper *OpenXRMetaEnvironmentDepthExtensionWrapper::get_singleton() {
+	if (singleton == nullptr) {
+		singleton = memnew(OpenXRMetaEnvironmentDepthExtensionWrapper());
+	}
+	return singleton;
+}
+
+OpenXRMetaEnvironmentDepthExtensionWrapper::OpenXRMetaEnvironmentDepthExtensionWrapper() :
+		OpenXRExtensionWrapperExtension() {
+	ERR_FAIL_COND_MSG(singleton != nullptr, "An OpenXRMetaEnvironmentDepthExtensionWrapper singleton already exists.");
+
+	request_extensions[XR_META_ENVIRONMENT_DEPTH_EXTENSION_NAME] = &meta_environment_depth_ext;
+	singleton = this;
+}
+
+OpenXRMetaEnvironmentDepthExtensionWrapper::~OpenXRMetaEnvironmentDepthExtensionWrapper() {
+	cleanup();
+}
+
+void OpenXRMetaEnvironmentDepthExtensionWrapper::_bind_methods() {
+	// @todo
+}
+
+void OpenXRMetaEnvironmentDepthExtensionWrapper::cleanup() {
+	meta_environment_depth_ext = false;
+}
+
+Dictionary OpenXRMetaEnvironmentDepthExtensionWrapper::_get_requested_extensions() {
+	Dictionary result;
+	for (auto ext : request_extensions) {
+		uint64_t value = reinterpret_cast<uint64_t>(ext.value);
+		result[ext.key] = (Variant)value;
+	}
+	return result;
+}
+
+void OpenXRMetaEnvironmentDepthExtensionWrapper::_on_instance_created(uint64_t instance) {
+	if (meta_environment_depth_ext) {
+		bool result = initialize_meta_environment_depth_extension((XrInstance)instance);
+		if (!result) {
+			UtilityFunctions::print("Failed to initialize XR_META_environment_depth extension");
+			meta_environment_depth_ext = false;
+		}
+	}
+}
+
+void OpenXRMetaEnvironmentDepthExtensionWrapper::_on_instance_destroyed() {
+	cleanup();
+}
+
+bool OpenXRMetaEnvironmentDepthExtensionWrapper::initialize_meta_environment_depth_extension(const XrInstance &p_instance) {
+	GDEXTENSION_INIT_XR_FUNC_V(xrCreateEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrDestroyEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrStartEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrStopEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrCreateEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrDestroyEnvironmentDepthProviderMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrEnumerateEnvironmentDepthSwapchainImagesMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrGetEnvironmentDepthSwapchainStateMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrAcquireEnvironmentDepthImageMETA);
+	GDEXTENSION_INIT_XR_FUNC_V(xrSetEnvironmentDepthHandRemovalMETA);
+
+	return true;
+}
---
+++ plugin/src/main/cpp/include/extensions/openxr_meta_environment_depth_extension_wrapper.h
@@ -0,0 +1,113 @@
+/**************************************************************************/
+/*  openxr_meta_environment_depth_extension_wrapper.h                     */
+/**************************************************************************/
+/*                       This file is part of:                            */
+/*                              GODOT XR                                  */
+/*                      https://godotengine.org                           */
+/**************************************************************************/
+/* Copyright (c) 2022-present Godot XR contributors (see CONTRIBUTORS.md) */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include <openxr/openxr.h>
+#include <godot_cpp/classes/open_xr_extension_wrapper_extension.hpp>
+#include <godot_cpp/templates/hash_map.hpp>
+
+#include "util.h"
+
+using namespace godot;
+
+// Wrapper for the Meta environment depth extension.
+class OpenXRMetaEnvironmentDepthExtensionWrapper : public OpenXRExtensionWrapperExtension {
+	GDCLASS(OpenXRMetaEnvironmentDepthExtensionWrapper, OpenXRExtensionWrapperExtension);
+
+public:
+	Dictionary _get_requested_extensions() override;
+
+	void _on_instance_created(uint64_t instance) override;
+	void _on_instance_destroyed() override;
+
+	bool is_environment_depth_supported() {
+		return meta_environment_depth_ext;
+	}
+
+	static OpenXRMetaEnvironmentDepthExtensionWrapper *get_singleton();
+
+	OpenXRMetaEnvironmentDepthExtensionWrapper();
+	~OpenXRMetaEnvironmentDepthExtensionWrapper();
+
+protected:
+	static void _bind_methods();
+
+private:
+	EXT_PROTO_XRRESULT_FUNC3(xrCreateEnvironmentDepthProviderMETA,
+			(XrSession), session,
+			(const XrEnvironmentDepthProviderCreateInfoMETA *), createInfo,
+			(XrEnvironmentDepthProviderMETA *), environmentDepthProvider);
+
+	EXT_PROTO_XRRESULT_FUNC1(xrDestroyEnvironmentDepthProviderMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider);
+
+	EXT_PROTO_XRRESULT_FUNC1(xrStartEnvironmentDepthProviderMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider);
+
+	EXT_PROTO_XRRESULT_FUNC1(xrStopEnvironmentDepthProviderMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider);
+
+	EXT_PROTO_XRRESULT_FUNC3(xrCreateEnvironmentDepthSwapchainMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider,
+			(const XrEnvironmentDepthSwapchainCreateInfoMETA *), createInfo,
+			(XrEnvironmentDepthSwapchainMETA *), swapchain);
+
+	EXT_PROTO_XRRESULT_FUNC1(xrDestroyEnvironmentDepthSwapchainMETA,
+			(XrEnvironmentDepthSwapchainMETA), swapchain);
+
+	EXT_PROTO_XRRESULT_FUNC4(xrEnumerateEnvironmentDepthSwapchainImagesMETA,
+			(XrEnvironmentDepthSwapchainMETA), swapchain,
+			(uint32_t), imageCapacityInput,
+			(uint32_t *), imageCountOutput,
+			(XrSwapchainImageBaseHeader *), images);
+
+	EXT_PROTO_XRRESULT_FUNC2(xrGetEnvironmentDepthSwapchainStateMETA,
+			(XrEnvironmentDepthSwapchainMETA), swapchain,
+			(XrEnvironmentDepthSwapchainStateMETA *), state);
+
+	EXT_PROTO_XRRESULT_FUNC3(xrAcquireEnvironmentDepthImageMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider,
+			(const XrEnvironmentDepthImageAcquireInfoMETA*), acquireInfo,
+			(XrEnvironmentDepthImageMETA *), environmentDepthImage);
+
+	EXT_PROTO_XRRESULT_FUNC2(xrSetEnvironmentDepthHandRemovalMETA,
+			(XrEnvironmentDepthProviderMETA), environmentDepthProvider,
+			(const XrEnvironmentDepthHandRemovalSetInfoMETA *), setInfo);
+
+	bool initialize_meta_environment_depth_extension(const XrInstance &instance);
+
+	HashMap<String, bool *> request_extensions;
+
+	void cleanup();
+
+	static OpenXRMetaEnvironmentDepthExtensionWrapper *singleton;
+
+	bool meta_environment_depth_ext = false;
+};
