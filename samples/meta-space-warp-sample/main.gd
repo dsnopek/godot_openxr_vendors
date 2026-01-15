4798bc9b0c0a19bc40ee8bdaf5a47cd2f98b5215
David Snopek <dsnopek@gmail.com>
2026-01-15 13:06:14 -0600

    Disable skip AppSW on snap turn and add static cube

--- samples/meta-space-warp-sample/main.gd
+++ samples/meta-space-warp-sample/main.gd
@@ -88,7 +88,9 @@ func check_turn(name: String, value: Vector2) -> void:
 				turn_timer.start()
 				var fb_space_warp = Engine.get_singleton("OpenXRFbSpaceWarpExtensionWrapper")
 				if fb_space_warp:
-					fb_space_warp.skip_space_warp_frame()
+					# Disable so that we can see the effect of appSpaceDeltaPose:
+					#fb_space_warp.skip_space_warp_frame()
+					pass
 	else:
 		smooth_turn_input = value.x
 
