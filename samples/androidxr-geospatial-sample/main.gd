db8ae4f27188610171ee4517131b7d924ccf2eb7
David Snopek <dsnopek@gmail.com>
2026-08-20 17:11:51 -0500

    Add floating panel (not just print statements)

--- samples/androidxr-geospatial-sample/main.gd
+++ samples/androidxr-geospatial-sample/main.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+const HUD = preload("res://hud.gd")
+
+@onready var viewport_2d_in_3d = %Viewport2Din3D
 @onready var camera: XRCamera3D = %XRCamera3D
 @onready var timer: Timer = %Timer
 
@@ -80,11 +83,22 @@ class LocationFinder:
 var location_finder: LocationFinder
 
 func _ready() -> void:
+	var hud: HUD = viewport_2d_in_3d.get_scene_root()
+
+	var geospatial_supported := OpenXRAndroidGeospatialExtension.is_geospatial_supported()
+	hud.geospatial_supported_field.text = str(geospatial_supported)
+	if not geospatial_supported:
+		print("Geospatial isn't supported.")
+		return
+
 	var authenticated := await authenticate_with_google_cloud()
+	hud.authenticated_field.text = str(authenticated)
 	print("Authenticated: ", authenticated)
 	if not authenticated:
 		return
 
+	# If we want to confirm VPS availability before starting Geospatial,
+	# then we need to get the location usual Android way.
 	location_finder = LocationFinder.new()
 	await location_finder.completed
 	if not location_finder.is_success():
@@ -100,6 +114,7 @@ func _ready() -> void:
 		print("Getting VPS didn't finish: ", vps_result.get_status())
 
 	var vps_available: bool = vps_result.get_result_value()
+	hud.vps_availability_field.text = str(vps_available)
 	print("VPS available: ", vps_available)
 	if not vps_available:
 		return
@@ -152,7 +167,23 @@ func _on_geospatial_state_changed(p_state) -> void:
 
 
 func _on_timer_timeout() -> void:
+	var hud: HUD = viewport_2d_in_3d.get_scene_root()
 	var pose: OpenXRAndroidGeospatialPose = OpenXRAndroidGeospatialExtension.transform_to_geospatial_pose(camera.transform)
+
+	var position_valid := pose.is_position_valid()
+	hud.position_valid_field.text = str(position_valid)
+	if position_valid:
+		hud.latitude_field.text = str(pose.latitude)
+		hud.longitude_field.text = str(pose.longitude)
+		hud.horizontal_accuracy_field.text = str(pose.horizontal_accuracy)
+		hud.vertical_accuracy_field.text = str(pose.vertical_accuracy)
+
+	var orientation_valid := pose.is_orientation_valid()
+	hud.orientation_valid_field.text = str(orientation_valid)
+	if orientation_valid:
+		hud.orientation_field.text = str(pose.orientation)
+		hud.orientation_yaw_accuracy_field.text = str(pose.orientation_yaw_accuracy)
+
 	print("Position valid: ", pose.is_position_valid())
 	print("Orientation valid: ", pose.is_orientation_valid())
 	print("-----")
